e6e962e0a6eda2cf19836b77d57e01cb7e0401e4
Paul Harrison <pfh@logarithmic.net>
2019-03-02 11:44:54 +1100

    Update package writing material, add instructions for cloud R

--- r-progtidy-files/programming.R
+++ r-progtidy-files/programming.R
@@ -432,16 +432,33 @@ source("fastqc.R")
 
 # Create an empty package template
 library(devtools)
-create("mypack")
+library(usethis)
 
-# ... Edit mylibrary/DESCRIPTION file
-# ... Write .R files in mylibrary/R folder
+create_package("mypack", open=FALSE)
+
+
+# ... Edit mypack/DESCRIPTION file
+# ... Write .R files in mypackR folder
+# For example create a file mypack/R/hello.R containing:
+#----
+
+#' @export
+hello <- function() {
+    cat("Hello, world.\n")
+}
+
+#----
 
 # Load package. Use this during development.
 load_all("mypack")
+hello()
 
-# Build package, including converting inline documentation to .Rd files using roxygen2.
+# Build package documentation, converting inline documentation to .Rd files using roxygen2.
+# Update NAMESPACE file (lists functions the package makes public).
+document("mypack")
+
 # Check for common problems and missing documentation.
+# This also automatically runs document( ).
 # A CRAN package must pass all checks.
 check("mypack")
 
